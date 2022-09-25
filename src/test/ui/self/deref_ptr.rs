18376ede9712d53e2801551586adc1d35295bff0
Adrian Taylor <adetaylor@chromium.org>
2022-09-25 09:49:52 +0100

    Fix some test typos

--- src/test/ui/self/deref_ptr.rs
+++ src/test/ui/self/deref_ptr.rs
@@ -1,19 +1,21 @@
 // run-pass
 
+#![feature(arbitrary_self_types)]
+
 struct Foo(u32);
 
 impl Foo {
-    fn contents(*const self) -> u32 {
+    fn contents(self: *const Foo) -> u32 {
         unsafe { self.as_ref() }.unwrap().0
     }
 }
 
-struct FooContainer<'a>(foo: &'a Foo);
+struct FooContainer<'a>(&'a Foo);
 
-impl core::ops::DerefPtr for FooContainer {
+impl core::ops::DerefPtr for FooContainer<'_> {
     type Target = Foo;
-    fn Deref(&self) -> *const Self::Target {
-        self.0 as *const Self::Target;
+    fn deref(&self) -> *const Self::Target {
+        self.0 as *const Self::Target
     }
 }
 
